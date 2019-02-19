a5d8c18f8b9074539678bbec6b1c4efa060034d4
Arthur Habicht <arthur.habicht@ti8m.ch>
2019-02-19 14:57:08 +0100

    bugfix

--- src/data/datasources.py
+++ src/data/datasources.py
@@ -9,8 +9,8 @@ class SKLearnDigits():
         self.X = scale(self.digits.data)
         self.y = self.digits.target
 
-        self.n_samples, n_features = self.data.shape
-        self.n_digits = len(np.unique(self.digits.target))
+        self.n_samples, self.n_features = self.X.shape
+        self.n_digits = len(np.unique(self.y))
 
         print("n_digits: %d, \t n_samples %d, \t n_features %d"
-              % (self.n_digits, self.n_samples, n_features))
+              % (self.n_digits, self.n_samples, self.n_features))
